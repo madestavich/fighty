6b14bc77ba47cbac55d5b999f87b63b7c2e0d6fa
Максим Ухань <karlmadest@gmail.com>
2022-06-17 20:17:35 +0300

    commit 17.06

--- js/code.js
+++ js/code.js
@@ -2,15 +2,32 @@
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+const drawLineWidth = document.getElementById("lineWidth");
+const canvasWidth = document.getElementById("canvasWidth");
+const canvasHeight = document.getElementById("canvasHeight");
+const drawLineColor = document.getElementById("lineColor");
+const canvasConfig = {
+  maxWidth: 800,
+  maxHeight: 800,
+  minWidth: 300,
+  minHeight: 300,
+  defaultWidth: 500,
+  defaultHeight: 700,
+};
+
+//?--------------------------------------
+
+canvas.width = canvasConfig.defaultWidth;
+canvas.height = canvasConfig.defaultHeight;
 
 //*----------------------------------------------------------------
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
 let isDrawing = false;
 let x = 0;
 let y = 0;
+let lineWidth = 2;
+let lineColor = "#000";
+//?--------------------------------------
 
 function lineStart(e) {
   e.preventDefault();
@@ -50,13 +67,15 @@ function lineEnd(e) {
 
 function drawLine(context, x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = "white";
-  context.lineWidth = 3;
+  context.strokeStyle = lineColor;
+  context.lineWidth = lineWidth;
   context.moveTo(x1, y1);
   context.lineTo(x2, y2);
   context.stroke();
   context.closePath();
 }
+
+//*----------------------------------------------------------------
 
 canvas.addEventListener("touchstart", (e) => {
   lineStart(e);
@@ -78,4 +97,32 @@ canvas.addEventListener("mousemove", (e) => {
 });
 canvas.addEventListener("mouseup", (e) => {
   lineEnd(e);
+});
+
+drawLineWidth.addEventListener("change", (e) => {
+  lineWidth = e.target.value;
+  console.log(lineWidth);
+});
+
+canvasWidth.addEventListener("change", (e) => {
+  canvas.width = e.target.value;
+});
+
+canvasHeight.addEventListener("change", (e) => {
+  canvas.height = e.target.value;
+});
+
+drawLineColor.addEventListener("change", (e) => {
+  lineColor = e.target.value;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (
+    e.clientX < canvas.offsetLeft ||
+    e.clientX > canvas.offsetLeft + canvas.width ||
+    e.clientY < canvas.offsetTop ||
+    e.clientY > canvas.offsetTop + canvas.height
+  ) {
+    isDrawing = false;
+  }
 });
